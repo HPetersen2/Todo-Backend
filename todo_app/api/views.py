dae9a4d2866ba7579142da530c6ddbaa70fcaedc
HPetersen2 <petermann2@web.de>
2025-12-22 14:19:52 +0100

    implementend views for todos

--- todo_app/api/views.py
+++ todo_app/api/views.py
@@ -1,11 +1,21 @@
 from rest_framework import generics
 from todo_app.models import Todo
+from .permissions import IsAuthenticatedWithCookie
 from .serializers import TodoSerializer
 
 class TodoListCreateView(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticatedWithCookie]
+
+    def get_queryset(self):
+        return Todo.objects.filter(creator=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
 class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticatedWithCookie]
+
+    def get_queryset(self):
+        return Todo.objects.filter(creator=self.request.user)
